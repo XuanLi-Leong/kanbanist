576688e80803a38ba227b5af81d72ebd3aacef16
Xuan Li Leong <xleong2@illinois.edu>
2018-11-12 17:41:41 -0600

    Modify some of the updateItem reducers to accept date_string

--- src/redux/middleware/todoist-persistence.js
+++ src/redux/middleware/todoist-persistence.js
@@ -90,8 +90,14 @@ const todoistPersistenceMiddleware = store => next => action => {
 
         case types.UPDATE_LIST_ITEM:
             function persistContentChange() {
-                const { item, text } = action.payload;
-                const updatedItem = { id: item.id, content: text };
+                const { item, text, date_string } = action.payload;
+                let updatedItem;
+                if (date_string === undefined) {
+                    updatedItem = { id: item.id, content: text };
+                } else {
+                    // We still POST if it is empty string
+                    updatedItem = { id: item.id, content: text, date_string: date_string };
+                }
                 Todoist.updateItem(token, updatedItem);
             }
             persistContentChange();
