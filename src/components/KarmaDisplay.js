b457516df66ef88c0900aeafad7aee78cca92407
Xuan Li Leong <xleong2@illinois.edu>
2018-11-13 15:21:08 -0600

    Update manual test plan

--- src/components/KarmaDisplay.js
+++ src/components/KarmaDisplay.js
@@ -1,3 +1,4 @@
+// Karma component in the header
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { AnchorButton, Icon, Intent, Tag } from '@blueprintjs/core';
