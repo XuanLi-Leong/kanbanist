439eabde97c7275f8cc1c7345a0d2e51e856994e
Xuan Li Leong <xleong2@illinois.edu>
2018-11-12 19:07:24 -0600

    Add skeleton for productivity stats - Create api function for fetching additional productivity stats - Stub out redux file for managing karma store

---
+++ src/redux/modules/karma.js
@@ -0,0 +1,408 @@
+// Should listen to the getStats action
+const response = {
+    karma_last_update: 26,
+    karma_trend: 'up',
+    // PLOT THE GRAPH DATA
+    karma_graph_data: [
+        {
+            date: '2018-11-06',
+            karma_avg: 20317,
+        },
+        {
+            date: '2018-11-07',
+            karma_avg: 20321,
+        },
+        {
+            date: '2018-11-08',
+            karma_avg: 20324,
+        },
+        {
+            date: '2018-11-09',
+            karma_avg: 20352,
+        },
+        {
+            date: '2018-11-10',
+            karma_avg: 20354,
+        },
+        {
+            date: '2018-11-11',
+            karma_avg: 20356,
+        },
+        {
+            date: '2018-11-12',
+            karma_avg: 20362,
+        },
+    ],
+    // LABEL EACH PLOT POINT WITH NUMBER OF TASKS COMPLETED
+    days_items: [
+        {
+            date: '2018-11-12',
+            items: [
+                {
+                    completed: 11,
+                    id: 128829961,
+                },
+            ],
+            total_completed: 11,
+        },
+        {
+            date: '2018-11-11',
+            items: [
+                {
+                    completed: 1,
+                    id: 2192757754,
+                },
+                {
+                    completed: 1,
+                    id: 2192757780,
+                },
+                {
+                    completed: 2,
+                    id: 2195421391,
+                },
+            ],
+            total_completed: 4,
+        },
+        {
+            date: '2018-11-10',
+            items: [
+                {
+                    completed: 1,
+                    id: 161248860,
+                },
+            ],
+            total_completed: 1,
+        },
+        {
+            date: '2018-11-09',
+            items: [
+                {
+                    completed: 1,
+                    id: 2192757754,
+                },
+            ],
+            total_completed: 1,
+        },
+        {
+            date: '2018-11-08',
+            items: [
+                {
+                    completed: 1,
+                    id: 128829961,
+                },
+                {
+                    completed: 2,
+                    id: 184695671,
+                },
+                {
+                    completed: 2,
+                    id: 2192757757,
+                },
+                {
+                    completed: 1,
+                    id: 2192757760,
+                },
+                {
+                    completed: 2,
+                    id: 2195421391,
+                },
+            ],
+            total_completed: 8,
+        },
+        {
+            date: '2018-11-07',
+            items: [],
+            total_completed: 0,
+        },
+        {
+            date: '2018-11-06',
+            items: [
+                {
+                    completed: 1,
+                    id: 2192757754,
+                },
+                {
+                    completed: 1,
+                    id: 2193230069,
+                },
+            ],
+            total_completed: 2,
+        },
+    ],
+    completed_count: 2730,
+    // INVESTIGATE MEANING OF "KARMA UPDATE REASON"
+    karma_update_reasons: [
+        {
+            positive_karma_reasons: [10],
+            new_karma: 20389,
+            negative_karma: 0,
+            positive_karma: 26,
+            negative_karma_reasons: [],
+            time: 'Mon 12 Nov 2018 23:36:35 +0000',
+        },
+        {
+            positive_karma_reasons: [9],
+            new_karma: 20363,
+            negative_karma: 0,
+            positive_karma: 6,
+            negative_karma_reasons: [],
+            time: 'Mon 12 Nov 2018 19:00:10 +0000',
+        },
+        {
+            positive_karma_reasons: [9],
+            new_karma: 20357,
+            negative_karma: 0,
+            positive_karma: 6,
+            negative_karma_reasons: [],
+            time: 'Sun 11 Nov 2018 21:48:17 +0000',
+        },
+        {
+            positive_karma_reasons: [10],
+            new_karma: 20351,
+            negative_karma: 0,
+            positive_karma: 26,
+            negative_karma_reasons: [],
+            time: 'Thu 08 Nov 2018 21:45:42 +0000',
+        },
+        {
+            positive_karma_reasons: [9],
+            new_karma: 20325,
+            negative_karma: 0,
+            positive_karma: 6,
+            negative_karma_reasons: [],
+            time: 'Thu 08 Nov 2018 18:59:58 +0000',
+        },
+        {
+            positive_karma_reasons: [9],
+            new_karma: 20319,
+            negative_karma: 0,
+            positive_karma: 6,
+            negative_karma_reasons: [],
+            time: 'Wed 07 Nov 2018 00:42:44 +0000',
+        },
+        {
+            positive_karma_reasons: [9],
+            new_karma: 20313,
+            negative_karma: 0,
+            positive_karma: 6,
+            negative_karma_reasons: [],
+            time: 'Mon 05 Nov 2018 23:54:42 +0000',
+        },
+    ],
+    karma: 20389,
+    week_items: [
+        {
+            date: '2018-11-12/2018-11-18',
+            items: [
+                {
+                    completed: 11,
+                    id: 128829961,
+                },
+            ],
+            total_completed: 11,
+        },
+        {
+            date: '2018-11-05/2018-11-11',
+            items: [
+                {
+                    completed: 1,
+                    id: 128829961,
+                },
+                {
+                    completed: 2,
+                    id: 161248860,
+                },
+                {
+                    completed: 2,
+                    id: 184695671,
+                },
+                {
+                    completed: 1,
+                    id: 2154910561,
+                },
+                {
+                    completed: 3,
+                    id: 2192757754,
+                },
+                {
+                    completed: 2,
+                    id: 2192757757,
+                },
+                {
+                    completed: 1,
+                    id: 2192757760,
+                },
+                {
+                    completed: 1,
+                    id: 2192757780,
+                },
+                {
+                    completed: 1,
+                    id: 2193230069,
+                },
+                {
+                    completed: 5,
+                    id: 2195421391,
+                },
+            ],
+            total_completed: 19,
+        },
+        {
+            date: '2018-10-29/2018-11-04',
+            items: [
+                {
+                    completed: 5,
+                    id: 128829961,
+                },
+                {
+                    completed: 5,
+                    id: 128829963,
+                },
+                {
+                    completed: 4,
+                    id: 161248860,
+                },
+                {
+                    completed: 1,
+                    id: 178940016,
+                },
+                {
+                    completed: 1,
+                    id: 184695671,
+                },
+                {
+                    completed: 2,
+                    id: 2154910561,
+                },
+                {
+                    completed: 6,
+                    id: 2192757754,
+                },
+                {
+                    completed: 8,
+                    id: 2192757757,
+                },
+                {
+                    completed: 2,
+                    id: 2192757760,
+                },
+                {
+                    completed: 4,
+                    id: 2192757780,
+                },
+                {
+                    completed: 5,
+                    id: 2193230069,
+                },
+                {
+                    completed: 5,
+                    id: 2193482592,
+                },
+                {
+                    completed: 7,
+                    id: 2195421391,
+                },
+            ],
+            total_completed: 55,
+        },
+        {
+            date: '2018-10-22/2018-10-28',
+            items: [
+                {
+                    completed: 3,
+                    id: 128829961,
+                },
+                {
+                    completed: 1,
+                    id: 128829963,
+                },
+                {
+                    completed: 1,
+                    id: 161248860,
+                },
+                {
+                    completed: 1,
+                    id: 2154910561,
+                },
+                {
+                    completed: 6,
+                    id: 2192757754,
+                },
+                {
+                    completed: 5,
+                    id: 2192757757,
+                },
+                {
+                    completed: 1,
+                    id: 2192757760,
+                },
+                {
+                    completed: 4,
+                    id: 2193230069,
+                },
+                {
+                    completed: 3,
+                    id: 2193482592,
+                },
+                {
+                    completed: 10,
+                    id: 2195421391,
+                },
+            ],
+            total_completed: 35,
+        },
+    ],
+    project_colors: {
+        '128829961': 7,
+        '128829963': 0,
+        '161248860': 8,
+        '178940016': 0,
+        '184695671': 2,
+        '2154910561': 11,
+        '2192757754': 8,
+        '2192757757': 8,
+        '2192757760': 8,
+        '2192757780': 8,
+        '2193230069': 8,
+        '2193482592': 2,
+        '2195421391': 9,
+    },
+    goals: {
+        karma_disabled: 0,
+        user_id: 2613818,
+        last_daily_streak: {
+            count: 22,
+            start: '2018-10-12',
+            end: '2018-11-12',
+        },
+        max_weekly_streak: {
+            count: 41,
+            start: '2018-01-08',
+            end: '2018-11-18',
+        },
+        ignore_days: [5, 6, 7],
+        vacation_mode: 1,
+        current_weekly_streak: {
+            count: 41,
+            start: '2018-01-08',
+            end: '2018-11-18',
+        },
+        current_daily_streak: {
+            count: 22,
+            start: '2018-10-12',
+            end: '2018-11-12',
+        },
+        last_weekly_streak: {
+            count: 41,
+            start: '2018-01-08',
+            end: '2018-11-18',
+        },
+        weekly_goal: 10,
+        max_daily_streak: {
+            count: 22,
+            start: '2018-10-12',
+            end: '2018-11-12',
+        },
+        daily_goal: 2,
+    },
+};
